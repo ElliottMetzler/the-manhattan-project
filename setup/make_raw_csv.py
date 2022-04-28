ee5e3e0cc2a66d1c80184e34045c14010930bf38
PedroNBRodrigues <peperodrigues@me.com>
2022-04-28 15:03:21 +0000

    All drinks available on new csv

--- setup/make_raw_csv.py
+++ setup/make_raw_csv.py
@@ -9,6 +9,7 @@ import json
 if __name__ == "__main__":
     BASE_DIR = "data"
     CSV_PATH = os.path.join(BASE_DIR, "raw_data.csv")
+    CSV_PATH1 = os.path.join(BASE_DIR, "complete_raw_data.csv")
     os.makedirs(BASE_DIR, exist_ok=True)
     
 
@@ -35,3 +36,33 @@ df = pd.DataFrame(normalize)
 raw_data = pd.DataFrame(df.stack().apply(pd.Series))
 
 raw_data.to_csv(CSV_PATH)
+
+
+BASE_URL2 = ("http://www.thecocktaildb.com/api/json/v2/9973533/search.php?f=")
+
+NUM = list(string.digits)
+NUM.extend(ALPHA)
+NUM.remove("0")
+NUM.remove("8")
+
+page_links2 = []
+
+for n in NUM:
+    url2 = '{}{}'.format(BASE_URL2, n)
+    page_links2.append(url2)
+    
+try2 = []
+for url in page_links2:
+    response1 = requests.get(url)
+    response1.raise_for_status()
+    response1_json = response1.json()
+    try2.append(response1_json["drinks"])
+
+
+normalize1 = json_normalize(try2)
+df1 = pd.DataFrame(normalize1)
+raw_data1 = pd.DataFrame(df1.stack().apply(pd.Series))
+
+raw_data1.reset_index(drop=True, inplace=True)
+
+raw_data1.to_csv(CSV_PATH1)
