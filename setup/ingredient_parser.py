19df2552ee9954211771980baf57f9db07d99676
koneeb <koneeb@utexas.edu>
2022-04-28 19:28:26 +0000

    Updated parser file with patterns.

--- setup/ingredient_parser.py
+++ setup/ingredient_parser.py
@@ -3,270 +3,48 @@ import numpy as np
 import re
 import os
 
-if __name__ == "__main__":
-    BASE_DIR = "data"
-    IN_PATH = os.path.join(BASE_DIR, "raw_data.csv")
-    OUT_PATH = os.path.join(BASE_DIR, "parsed_data.csv")
-    os.makedirs(BASE_DIR, exist_ok=True)
 
-"""Extract ingredient measurement columns from the csv as a dataframe."""
-
-df = (
-    pd.read_csv(IN_PATH, index_col=None, header=None)
-    .iloc[:, 32:47]
-    .fillna(0)
-    .astype(str)
-     )
-
-"""Remove all numbers and create a list of unique values of units of measurements"""
-
-all_values = list(np.unique(df.values))
-
-no_numbers = [] 
-
-for unit in all_values:
-    no_numbers.append(re.sub("[^a-zA-Z ]","",unit).strip())
+def extract_columns(in_path):
+    """Extract ingredient measurement columns from the csv as a dataframe and remove commas and paranthesis."""
+    df = (
+        pd.read_csv(in_path, index_col=None, header=None)
+        .iloc[:, 32:47]
+        .fillna(0)
+        .astype(str)
+         )
+    for col in df.columns:
+        df[col]=df[col].str.replace(r"\(|\)|,", "")
+        
+    return df
     
-no_numbers = list(set(no_numbers))
+def create_units_list(dataframe):
+    """Remove all numbers and create a list of unique values of units of measurements"""
 
-"""Create a dictionary converting all units of measurement to ounces."""
+    all_values = list(np.unique(dataframe.values))
 
-liquid_units = {"":1,
-                "oz":1,
-                "ml":0.033814,
-                "cl":0.33814,
-                "tsp":0.166667,
-                "teaspoon":0.166667,
-                "teaspoons":0.166667,
-                "tea spoon":0.166667,
-                "tbsp":0.5,
-                "tablespoon":0.5,
-                "tablespoons":0.5,
-                "table spoon":0.5,
-                "cup":8,
-                "cups":8,
-                "qt":0.03125,
-                "quart":0.03125,
-                "quarts":0.03125,
-                "drop":0.0016907,
-                "drops":0.0016907,
-                "shot":1.5,
-                "shots":1.5,
-                "cube":1,
-                "cubes":1,
-                "dash":0.03125,
-                "dashes":0.03125,
-                "l":33.814,
-                "L":33.814,
-                "liters":33.814,
-                "Liters":33.814,
-                "wedge":0.125,
-                "wedges":0.125,
-                "pint":16,
-                "pints":16,
-                "slice":0.50721, 
-                "slices":0.50721, 
-                "twist of":0.0705479,
-                "top up":1,
-                "small bottle":7,
-                "Smirnoff red label":25.3605,
-                "Juice of":1,
-                "Fresh":1
-                
-               }
-# def append_dict_value():
-#     """Creates a dictionary for converting measurments units to ounces."""
-# dict_value_list = [
-#      1,
-#      0.0705479,
-#      1,
-#      8,
-#      1,
-#      8,
-#      1,
-#      0.125,
-#      0.033814,
-#      1,
-#      1,
-#      3.3814,
-#      1,
-#      0.0016907,
-#      0.5,
-#      12,
-#      1.5,
-#      8,
-#      0.033814,
-#      0.035,
-#      8,
-#      0.5,
-#      0.105822,
-#      0.166667,
-#      0.5,
-#      1,
-#      0.0016907,
-#      12,
-#      8,
-#      0.5,
-#      0.125,
-#      8,
-#      8,
-#      0.166667,
-#      3.2,
-#      0.2,
-#      1,
-#      0.26,
-#      0.33814,
-#      0.035274,
-#      0.010,
-#      1,
-#      12,
-#      12.6803,
-#      6,
-#      6,
-#      0.2,
-#      1,
-#      33.814,
-#      1,
-#      33.814,
-#      8,
-#      0.166667,
-#      1,
-#      1,
-#      0.50721,
-#      0.33814,
-#      0.50721,
-#      6,
-#      0.554113,
-#      1.5,
-#      1,
-#      0.375,
-#      0.166667,
-#      0.010,
-#      8,
-#      1,
-#      8,
-#      0.03125,
-#      1,
-#      1,
-#      1,
-#      1,
-#      1,
-#      5,
-#      1,
-#      0.166667,
-#      1,
-#      0.166667,
-#      8,
-#      1,
-#      0.03125,
-#      12,
-#      0.31,
-#      'piece': None,
-#      'seltzer water': None,
-#      'measures': None,
-#      'Chilled': None,
-#      1,
-#      'pods': None,
-#      8,
-#      'very sweet': None,
-#      'gal': None,
-#      8,
-#      8,
-#      'Fill to top with': None,
-#      'mini': None,
-#      'to taste': None,
-#      0.010,
-#      'ground': None,
-#      1,
-#      0.166667,
-#      0.03125,
-#      8,
-#      0.125,
-#      1.5,
-#      'frozen': None,
-#      'whole': None,
-#      1,
-#      0.010,
-#      'inch strips': None,
-#      0.166667,
-#      1,
-#      'pieces': None,
-#      'scoops': None,
-#      'Fill with': None,
-#      0.033814,
-#      0.333333,
-#      8,
-#      'long strip': None,
-#      16,
-#      0.0016907,
-#      0.5,
-#      8,
-#      0.0705479,
-#      8,
-#      0.5,
-#      6,
-#      0.5,
-#      8,
-#      1: None,
-#      1,
-#      1,
-#      0.166667,
-#      1.5,
-#      0.125,
-#      'Top': None,
-#      1,
-#      0.2,
-#      16,
-#      'Unsweetened': None,
-#      3.3814,
-#      0.5,
-#      'Over': None,
-#      8,
-#      33.814,
-#      1,
-#      1,
-#      8,
-#      7,
-#      'Claret': None,
-#      1,
-#      1,
-#      0.03125,
-#      1,
-#      8,
-#      'Squeeze': None,
-#      1,
-#      1.5,
-#      'black': None,
-#      0.5,
-#      0.033814,
-#      0.166667,
-#      1,
-#      1,
-#      1,
-#      5.08,
-#      0.31,
-#      1.5,
-#      0.33814,
-#      0.5,
-#      1.5,
-#      'piece textural': None,
-#      'Ground': None,
-#      1,
-#      1,
-#      14,
-#      0.03125,
-#      12,
-#      25.4,
-#      1,
-#      'stick': None,
-#      1]
-# 
-# dictionary = dict(zip(no_numbers, dict_value_list))
+    units_list = [] 
+
+    for unit in all_values:
+        units_list.append(re.sub("[^a-zA-Z ]","",unit).strip())
+
+    units_list = list(set(units_list))
+    
+    return units_list
+
+
+def append_dict_value(unit_list):
+    """Creates a dictionary for converting measurments units to ounces."""
+    
+    dict_value_list = [1, 0.0705479, 1, 8, 1, 8, 1, 0.125, 0.033814, 1, 1, 3.3814, 1, 0.0016907, 0.5, 12, 1.5, 8, 0.033814, 0.035, 8, 0.5, 0.105822, 0.166667, 0.5, 1, 0.0016907, 12, 8, 0.5, 0.125, 8, 8, 0.166667, 3.2, 0.2, 1, 0.26, 0.33814, 0.035274, 0.010, 1, 12, 12.6803, 6, 6, 0.2, 1, 33.814, 1, 33.814, 8, 0.166667, 1, 1, 0.50721, 0.33814,0.50721, 6, 0.554113, 1.5, 1, 0.375, 0.166667, 0.010, 8, 1, 8, 0.03125, 1, 1, 1, 1, 1, 5, 1, 0.166667, 1, 0.166667, 8, 1, 0.03125, 12, 0.31, 0.0705479, 7.5, 1.5, 1, 1, 0.035, 8, 0.105822, 128, 8, 8, 1, 1, 0.01, 0.010, 0.01, 1, 0.166667, 0.03125, 8, 0.125, 1.5, 3, 2, 1, 0.010, 1, 0.166667, 1, 0.26, 3, 1, 0.033814, 0.333333, 8, 1, 16, 0.0016907, 0.5, 8, 0.0705479, 8, 0.5, 6, 0.5, 8, 1, 1, 1, 0.166667, 1.5, 0.125, 1, 1, 0.2, 16, 0.166667, 3.3814, 0.5, 3, 8, 33.814, 1, 1, 8, 7, 1, 1, 1, 0.03125, 1, 8, 1, 1, 1.5, 2, 0.5, 0.033814, 0.166667, 1, 1, 1, 5.08, 0.31, 1.5, 0.33814, 0.5, 1.5, 5, 0.01, 1, 1, 14, 0.03125, 12, 25.4, 1, 0.0705479, 1]
+
+    convert_dict = dict(zip(unit_list, dict_value_list))
+    return convert_dict
+
 
 def frac_to_dec_converter(num_strings):
     """Takes a list of strings that contains fractions and convert them into floats."""
     
-    result = []
+    result_list = []
 
     for frac_str in num_strings:
         try:
@@ -281,20 +59,22 @@ def frac_to_dec_converter(num_strings):
             frac = float(num) / float(denom)
             converted = total + frac
 
-        result.append(converted)
+        result_list.append(converted)
         
-    return result
+    return result_list
 
 def make_pattern(str_list):
     """Divides string list into readable pattern for regex."""
+    
     str_pattern = ""
     for string in str_list:
         str_pattern += f"{string}|" 
     return str_pattern
 
-def unit_unify(list_of_texts):
+def unit_unify(list_of_texts, unit_dict):
     """Takes a list of strings that contains liquid units, and converts them into fluid ounces."""
-    str_pattern = make_pattern(no_numbers)
+    
+    str_pattern = make_pattern(units_list)
     pattern = fr"(^[\d -/]*)({str_pattern})"
 
 
@@ -305,9 +85,10 @@ def unit_unify(list_of_texts):
         
         if re_result:
             amount = re_result.group(1).strip()
-            if not amount:
-                amount = "1.0"
             unit = re_result.group(2).strip()
+            
+            if not amount:
+                amount = "1"
 
             if "-" in amount:
                 ranged = True
@@ -321,9 +102,9 @@ def unit_unify(list_of_texts):
             amount = np.sum(amount_in_dec)
             
             if ranged:
-                to_oz = (amount*liquid_units[unit])/2
+                to_oz = (amount*unit_dict[unit])/2
             else:
-                to_oz = amount*liquid_units[unit]
+                to_oz = amount*unit_dict[unit]
 
             new_list.append(str(round(to_oz,2)))
 
@@ -332,9 +113,22 @@ def unit_unify(list_of_texts):
             
     return new_list
 
-"""Convert units within each measurement column and save results to a csv."""
+def convert_columns(dataframe, unit_dict, out_path):
+    """Convert units within each measurement column and save results to a csv."""
 
-for i in df.columns:
-    df[i] = unit_unify(df[i])
+    for i in dataframe.columns:
+        dataframe[i] = unit_unify(dataframe[i], unit_dict)
+
+    dataframe.to_csv(out_path, index=False, header=False)
+
+
+if __name__ == "__main__":
+    BASE_DIR = "data"
+    IN_PATH = os.path.join(BASE_DIR, "raw_data1.csv")
+    OUT_PATH = os.path.join(BASE_DIR, "parsed_data.csv")
+    os.makedirs(BASE_DIR, exist_ok=True)
     
-df.to_csv(OUT_PATH, index=False, header=False)
+    df = extract_columns(IN_PATH)
+    units_list = create_units_list(df)
+    convert_dict = append_dict_value(units_list)
+    convert_columns(df, convert_dict, OUT_PATH)
