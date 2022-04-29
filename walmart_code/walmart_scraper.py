0404368ffe877b8b7cfd3cca34d9564646f94709
galongoria <97994476+galongoria@users.noreply.github.com>
2022-04-29 13:31:09 -0500

    Code now gets prices from a search

--- walmart_code/walmart_scraper.py
+++ walmart_code/walmart_scraper.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-sq = 'salt'
-url = 'https://www.walmart.com/search?q=+sq'
+#url_file = 'https://raw.githubusercontent.com/ElliottMetzler/the-manhattan-project/get_data/data/ingredients.csv'
+#ingredients = pd.read_csv(url_file, header=None)
+# will use this later when the code is finished to run through the list
+
+
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0',
 'Accept': '*/*',
 'Accept-Language': 'en-US,en;q=0.5',
@@ -11,29 +15,46 @@ header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Geck
 'Origin': 'https://www.walmart.com',
 'Connection': 'keep-alive',
 'Referer': 'https://www.walmart.com/',
-'Cookie': 's=93a1e136612fc2a290edf2020a2bc2f8; U=e33b391aebc7946c7f4ae8b7b120d8e5',
+'Cookie': 'U=e33b391aebc7946c7f4ae8b7b120d8e5; s=b09113a20b79b2bdde3523b9467cdd73',
 'TE': 'trailers',
 'Sec-Fetch-Dest': 'empty',
 'Sec-Fetch-Mode': 'cors',
 'Sec-Fetch-Site': 'cross-site'}
 
+#change cookies periodically
+
+
+cookie = 'U=e33b391aebc7946c7f4ae8b7b120d8e5; s=b09113a20b79b2bdde3523b9467cdd73'
 def get_wm_search(query):
-    url = "https://www.walmart.com/search?q="+sq
+""" Takes a word and returns the search page on Walmart's website"""
+    url = "https://www.walmart.com/search?q="+query
     page = requests.get(url, headers=header)
     if page.status_code == 200:
         return page
     else:
         return "Error"
 
-def get_prod_link(soup):
-    link_list = []
-    link = soup.find_all('div', {'class': 'sans-serif mid-gray relative flex flex-column w-100'})
-    for l in link:
-        link_list.append('https://www.walmart.com'+l.a['href'])
-    return link_list
+# test vodka to make sure it's not taking other items' prices that are suggested from search
+def get_page_prices(soup):
+""" Right now this takes the salt search page and returns prices.  """
+    price_list = []
+    # the 'response' will eventually be a list of soup.contents
+    response = get_wm_search('vodka')
+    soup = BeautifulSoup(response.content)
+    page_results = soup.find_all('div', {'class': 'flex flex-wrap justify-start items-center lh-title mb2 mb1-m'})
+    for p in page_results:
+        price_oz = p.find('div', {'class': 'f7 f6-l gray mr1'}).text
+        price_list.append(price_oz)
+    return price_list
 
 
 
-response = get_wm_search('salt')
-soup = BeautifulSoup(response.content)
-links = get_prod_link(soup)
+def get_item_searches(ingredients):
+    """ Takes the list of ingredients and makes a list of soups. Scared to test this, please do not test it yet. """
+    queries = []
+    search_list = []
+    for i in ingredients:
+        response = get_wm_search(i)
+        soup = BeautifulSoup(response.content)
+        search_list.append(soup)
+    return search
