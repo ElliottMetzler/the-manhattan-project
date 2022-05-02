deded07a9d36021f68e0a4e15e2d8bead9247892
galongoria <97994476+galongoria@users.noreply.github.com>
2022-05-01 21:28:30 -0500

    can scrape small lists now

--- walmart_code/walmart_scraper.py
+++ walmart_code/walmart_scraper.py
@@ -24,6 +24,12 @@ header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Geck
 #change cookies periodically
 
 
+
+# the actual file is on a different repo so ill replace this github link with the csv once its there
+url_file = 'https://raw.githubusercontent.com/ElliottMetzler/the-manhattan-project/get_data/data/ingredients.csv'
+ingredients = pd.read_csv(url_file, header=None)
+
+
 cookie = 'U=e33b391aebc7946c7f4ae8b7b120d8e5; s=b09113a20b79b2bdde3523b9467cdd73'
 def get_wm_search(query):
 """ Takes a word and returns the search page on Walmart's website"""
@@ -33,20 +39,6 @@ def get_wm_search(query):
         return page
     else:
         return "Error"
-
-# test vodka to make sure it's not taking other items' prices that are suggested from search
-def get_page_prices(soup):
-""" Right now this takes the salt search page and returns prices.  """
-    price_list = []
-    # the 'response' will eventually be a list of soup.contents
-    response = get_wm_search('vodka')
-    soup = BeautifulSoup(response.content)
-    page_results = soup.find_all('div', {'class': 'flex flex-wrap justify-start items-center lh-title mb2 mb1-m'})
-    for p in page_results:
-        price_oz = p.find('div', {'class': 'f7 f6-l gray mr1'}).text
-        price_list.append(price_oz)
-    return price_list
-
 
 
 def get_item_searches(ingredients):
@@ -58,3 +50,14 @@ def get_item_searches(ingredients):
         soup = BeautifulSoup(response.content)
         search_list.append(soup)
     return search
+
+
+    # test vodka to make sure it's not taking other items' prices that are suggested from search
+def get_page_prices(soup):
+    price_list = []
+    soup = BeautifulSoup(response.content)
+    page_results = soup.find_all('div', {'class': 'flex flex-wrap justify-start items-center lh-title mb2 mb1-m'})
+    for p in page_results:
+        price_oz = p.find('div', {'class': 'f7 f6-l gray mr1'}).text
+        price_list.append(price_oz)
+    return price_list
