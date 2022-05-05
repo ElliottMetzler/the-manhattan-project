e4f94424a832cfa4c0b9b724ce73c7d3906cdf6c
Colin McNally <colinmcnally@colins-mbp.attlocal.net>
2022-05-05 14:30:07 -0500

    Making 5 Alternate Drinks

--- code/streamlitty.py
+++ code/streamlitty.py
@@ -156,4 +156,12 @@ with st.container():
 	col1.header("""If you aren't feeling our featured cocktail maybe one of these would be more your speed:""")
 	alt_sample=df.sample(5)
 	alt_1=alt_sample["strdrink"].value[0]
+	alt_2=alt_sample["strdrink"].value[1]
+	alt_3=alt_sample["strdrink"].value[2]
+	alt_4=alt_sample["strdrink"].value[3]
+	alt_5=alt_sample["strdrink"].value[4]
 	st.checkbox(f"Option 1:{alt_1}")
+	st.checkbox(f"Option 1:{alt_2}")
+	st.checkbox(f"Option 1:{alt_3}")
+	st.checkbox(f"Option 1:{alt_4}")
+	st.checkbox(f"Option 1:{alt_5}")
