c955f37070790b91e8c94e50e3318d76e96b8561
Colin McNally <colinmcnally@colins-mbp.attlocal.net>
2022-05-05 14:23:11 -0500

    Creating Containers

--- code/streamlitty.py
+++ code/streamlitty.py
@@ -65,89 +65,89 @@ alt_ingredient = gen_ingredient_selectbox()
 #######################################
 # First Column
 #######################################
+with st.container():
+	col1.header("Welcome to our Page")
 
-col1.header("Welcome to our Page")
+	col1.write("""
+		Hello! Welcome to The Manhattan Project. This webpage is designed to help fuel Friday night. We get it, you've had a long week, you don't want to go to the store, and you sure as hell don't want to think too hard about what you want to drink to unwind tonight. That's where we come in. 
 
-col1.write("""
-	Hello! Welcome to The Manhattan Project. This webpage is designed to help fuel Friday night. We get it, you've had a long week, you don't want to go to the store, and you sure as hell don't want to think too hard about what you want to drink to unwind tonight. That's where we come in. 
+		With our site, you can search through a vast database of cocktails and concoctions using a variety of search parameters and settings to find exactly what you're looking for - whether thats a night in with a movie and a bowl of popcorn, a night out with some strangers, or a Saturday morning tailgate.
 
-	With our site, you can search through a vast database of cocktails and concoctions using a variety of search parameters and settings to find exactly what you're looking for - whether thats a night in with a movie and a bowl of popcorn, a night out with some strangers, or a Saturday morning tailgate.
+		How to use: First, you will use the side panel on to select a search setting. We currently support a few options. You can search by glass type, which is often indicative of the type of drink (i.e. shot glass or punch bowl ought to produce a nice party). Alternatively, you can search by an ingredient you have on hand and would like to use up, with a filter for the number of ingredients in the cocktail. We also support a combination of the two previous search options. Finally, we can generate a completely random drink.
 
-	How to use: First, you will use the side panel on to select a search setting. We currently support a few options. You can search by glass type, which is often indicative of the type of drink (i.e. shot glass or punch bowl ought to produce a nice party). Alternatively, you can search by an ingredient you have on hand and would like to use up, with a filter for the number of ingredients in the cocktail. We also support a combination of the two previous search options. Finally, we can generate a completely random drink.
+		The panel to the right will display the featured result based on your search. Below, we also list other cocktails that fit the search parameters (if they exist), and allow you to select if you'd like to view another option.
+		""")
 
-	The panel to the right will display the featured result based on your search. Below, we also list other cocktails that fit the search parameters (if they exist), and allow you to select if you'd like to view another option.
-	""")
+	#######################################
+	# Second Column
+	#######################################
 
-#######################################
-# Second Column
-#######################################
+	col2.header("Featured Drink Result")
 
-col2.header("Featured Drink Result")
+	# Query Database and filter based on criteria
+	df = main_query(num_ingredients_tot)
 
-# Query Database and filter based on criteria
-df = main_query(num_ingredients_tot)
+	# Create Contains (Or) based on Main Boozes
+	booze_criteria = []
+	if vodka:
+		booze_criteria.append("vodka")
+	if whiskey:
+		booze_criteria.append("whiskey")
+	if tequila:
+		booze_criteria.append("tequila")
+	if mezcal:
+		booze_criteria.append("mezcal")
+	if gin:
+		booze_criteria.append("gin")
 
-# Create Contains (Or) based on Main Boozes
-booze_criteria = []
-if vodka:
-	booze_criteria.append("vodka")
-if whiskey:
-	booze_criteria.append("whiskey")
-if tequila:
-	booze_criteria.append("tequila")
-if mezcal:
-	booze_criteria.append("mezcal")
-if gin:
-	booze_criteria.append("gin")
+	# If none are selected, don't filter
+	if len(booze_criteria) > 0:
+		booze_mask = df["ingredients_list"].str.contains("|".join(booze_criteria))
+	else:
+		booze_mask = pd.Series([True] * len(df.index))
 
-# If none are selected, don't filter
-if len(booze_criteria) > 0:
-	booze_mask = df["ingredients_list"].str.contains("|".join(booze_criteria))
-else:
-	booze_mask = pd.Series([True] * len(df.index))
+	# If N/A is selected, don't filter
+	if alt_ingredient != "N/A":
+		alt_ingredient_mask = df["ingredients_list"].str.contains(alt_ingredient)
+	else:
+		alt_ingredient_mask = pd.Series([True] * len(df.index))
 
-# If N/A is selected, don't filter
-if alt_ingredient != "N/A":
-	alt_ingredient_mask = df["ingredients_list"].str.contains(alt_ingredient)
-else:
-	alt_ingredient_mask = pd.Series([True] * len(df.index))
+	# Perform data filtering
+	df = df[booze_mask & alt_ingredient_mask]
 
-# Perform data filtering
-df = df[booze_mask & alt_ingredient_mask]
+	# Sample a random drink from the list
+	featured_drink = df.sample(1)
 
-# Sample a random drink from the list
-featured_drink = df.sample(1)
+	name = featured_drink["strdrink"].values[0]
+	glass = featured_drink["strglass"].values[0]
+	instructions = featured_drink["strinstructions"].values[0]
+	image = featured_drink["strdrinkthumb"].values[0]
 
-name = featured_drink["strdrink"].values[0]
-glass = featured_drink["strglass"].values[0]
-instructions = featured_drink["strinstructions"].values[0]
-image = featured_drink["strdrinkthumb"].values[0]
+	ingredients_list = featured_drink["ingredients_list"].values[0].split(",")
+	proportions_list = featured_drink["proportions_list"].values[0].split(",")
 
-ingredients_list = featured_drink["ingredients_list"].values[0].split(",")
-proportions_list = featured_drink["proportions_list"].values[0].split(",")
+	col2.write(f"""
+		
+	Congratulations! You have selected the **{name}**!
 
-col2.write(f"""
-	
-Congratulations! You have selected the **{name}**!
+	First, you will need to get out a **{glass}**.
 
-First, you will need to get out a **{glass}**.
+	Next, grab the following ingredients:
+	"""
+	)
 
-Next, grab the following ingredients:
-"""
-)
+	for prop, ing in zip(proportions_list, ingredients_list):
+		col2.write(f"* {prop} {ing}")
 
-for prop, ing in zip(proportions_list, ingredients_list):
-	col2.write(f"* {prop} {ing}")
+	col2.write(f"""
+		Finally, here are the instructions to make your cocktail!
 
-col2.write(f"""
-	Finally, here are the instructions to make your cocktail!
+		{instructions}
 
-	{instructions}
+		If it looks anything like this, you're probably in good shape!
 
-	If it looks anything like this, you're probably in good shape!
+		""")
 
-	""")
+	col2.image(image)
 
-col2.image(image)
-
-col2.write("Cheers!")
+	col2.write("Cheers!")
